412797c6b934eb839901728e0b9108c8d93c05e5
Starle21 <staronova.lenka@gmail.com>
2021-06-24 17:05:29 +0200

    Part1.4 course information step4 refactor into array of objects final

--- part1/course_information/src/App.js
+++ part1/course_information/src/App.js
@@ -26,33 +26,35 @@ const Total = props => {
   return (
     <p>
       Number of exercises:{' '}
-      {props.exercises[0].exercises +
-        props.exercises[1].exercises +
-        props.exercises[2].exercises}{' '}
+      {props.parts[0].exercises +
+        props.parts[1].exercises +
+        props.parts[2].exercises}{' '}
     </p>
   );
 };
 
 const App = () => {
   const course = 'Half stack application development';
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10,
-  };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7,
-  };
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      name: 'State of a component',
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={[part1, part2, part3]} />
-      <Total exercises={[part1, part2, part3]} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
