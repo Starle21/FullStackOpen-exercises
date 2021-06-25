08689e5b4a5b92edb59f5cd02f3428573dc8c20b
Starle21 <staronova.lenka@gmail.com>
2021-06-25 21:08:34 +0200

    Part1.6 unicafe step1 final

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,11 +1,36 @@
 import React, { useState } from 'react';
 
-function App() {
+const Title = ({ text }) => {
+  return <h1>{text}</h1>;
+};
+
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const App = () => {
   const [bad, setBad] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [good, setGood] = useState(0);
 
-  return <div> sth goes here </div>;
-}
+  const giveFeedback = type => {
+    if (type === 'good') return () => setGood(good + 1);
+    if (type === 'bad') return () => setBad(bad + 1);
+    if (type === 'neutral') return () => setNeutral(neutral + 1);
+  };
+
+  return (
+    <div>
+      <Title text="give feedback" />
+      <Button handleClick={giveFeedback('good')} text="good" />
+      <Button handleClick={giveFeedback('neutral')} text="neutral" />
+      <Button handleClick={giveFeedback('bad')} text="bad" />
+      <Title text="statistics" />
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+    </div>
+  );
+};
 
 export default App;
