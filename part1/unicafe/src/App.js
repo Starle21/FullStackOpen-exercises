9b7c741a49e487f153f508225cee72ce1c01406d
Starle21 <staronova.lenka@gmail.com>
2021-06-25 19:44:47 +0200

    Part1.6 unicafe initial commit

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [bad, setBad] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [good, setGood] = useState(0);
+
+  return <div> sth goes here </div>;
+}
+
+export default App;
